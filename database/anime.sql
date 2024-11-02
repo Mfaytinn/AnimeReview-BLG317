ceefcbf0be71da91cc748aeb6be3e46786e49054
Mfaytinn <muhammedfaytin@gmail.com>
2024-11-02 23:56:28 +0300

    Create for 2 main table

---
+++ database/anime.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE anime_db;
+USE anime_db;
+CREATE DATABASE anime_db;
+USE anime_db;
+
+
+-- Table 2: Anime Information 
+CREATE TABLE Anime_Information (
+    anime_id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    english_name VARCHAR(255),
+    other_name VARCHAR(255),
+    synopsis TEXT,
+    type VARCHAR(50)
+);
+
+-- Table 3: Anime Scores
+CREATE TABLE Anime_Scores (
+    score_id INT PRIMARY KEY AUTO_INCREMENT,
+    anime_id INT,
+    score FLOAT,
+    rank INT,
+    popularity INT,
+    FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
+);
+
+
