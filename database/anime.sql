c5645026eb5d5b7d904843ed93abb5e4fd1121e8
Mfaytinn <muhammedfaytin@gmail.com>
2024-12-06 06:53:03 +0300

    Update on database structure

--- database/anime.sql
+++ database/anime.sql
@@ -4,7 +4,12 @@ USE anime_db;
 -- Table 1: Users
 CREATE TABLE Users (
     user_id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL
+    username VARCHAR(50) NOT NULL,
+    gender ENUM('Male', 'Female', 'Other'),
+    birthday DATE DEFAULT NULL,
+    place VARCHAR(50),
+    joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    days_watched FLOAT
 );
 
 -- Table 2: Anime Information 
@@ -14,16 +19,17 @@ CREATE TABLE Anime_Information (
     english_name VARCHAR(255),
     other_name VARCHAR(255),
     synopsis TEXT,
-    type_anime VARCHAR(50)
+    type_anime VARCHAR(20),
+    genres VARCHAR(255)
 );
 
 -- Table 3: Anime Scores
 CREATE TABLE Anime_Scores (
     score_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT,
-    username VARCHAR(50),
     anime_id INT,
-    score INT
+    score TINYINT,
+    comment TEXT,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
 );
@@ -31,23 +37,31 @@ CREATE TABLE Anime_Scores (
 -- Table 4: Producers
 CREATE TABLE Producers (
     producer_id INT PRIMARY KEY AUTO_INCREMENT,
-    producer_name TEXT
+    producer_name VARCHAR(255) NOT NULL
 );
 
 -- Table 5: Studios
 CREATE TABLE Studios (
     studio_id INT PRIMARY KEY AUTO_INCREMENT,
-    studio_name VARCHAR(255)
+    studio_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE licensors (
+    licensor_id INT PRIMARY KEY AUTO_INCREMENT,
+    licensor_name VARCHAR(255) NOT NULL
 );
 
 -- Table 6: Anime Production
 CREATE TABLE Anime_Production (
     production_id INT PRIMARY KEY AUTO_INCREMENT,
     anime_id INT,
-    -- studio_id INT,
-    licensors VARCHAR(255),
-    FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
-    FOREIGN KEY (studio_id) REFERENCES Studios(studio_id)
+    producer_id INT,
+    studio_id INT,
+    licensor_id INT,
+    FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id),
+    FOREIGN KEY (studio_id) REFERENCES Studios(studio_id),
+    FOREIGN KEY (producer_id) REFERENCES Producers(producer_id),
+    FOREIGN KEY (licensor_id) REFERENCES licensors(licensor_id)
 );
 
 -- Table 7: Anime Metadata
@@ -61,10 +75,12 @@ CREATE TABLE Anime_Metadata (
     FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
 );
 
+
+
 -- Table 8: Anime Genres
-CREATE TABLE Anime_Genres (
-    genre_id INT PRIMARY KEY AUTO_INCREMENT,
-    anime_id INT,
-    genres VARCHAR(255),
-    FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
-);
+-- CREATE TABLE Anime_Genres (
+--     genre_id INT PRIMARY KEY AUTO_INCREMENT,
+--     anime_id INT,
+--     genres VARCHAR(255),
+--     FOREIGN KEY (anime_id) REFERENCES Anime_Information(anime_id)
+-- );
